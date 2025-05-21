b8d25c6143a2416a6b9d39dbdf53f383f9997a31
veer708 <veerahirwal51@gmail.com>
2025-05-21 12:49:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -23,9 +23,9 @@ import re
 import os
 
 bot = Client("bot",
-             bot_token= "6739442523:AAHHA5HZmU6sqI-elmhTs8dwHqfhTizWwnQ",
-             api_id=22609670,
-             api_hash= "3506d8474ad1f4f5e79b7c52a5c3e88d")
+             bot_token= "",
+             api_id=29232353,
+             api_hash= "6868788228291767c90e4346eea03f36")
 
 
 @bot.on_message(filters.command(["start"]) & filters.user(ADMINS))
